9d334ab6b5da3fa6f9f8dd2aed3c30db5956637c
Pia <piiseli1956@gmail.com>
2022-01-09 15:17:17 +0100

    eka

---
+++ nimet.js
@@ -0,0 +1,6 @@
+var firstname = "Your first name";
+        var lastname = "Your lastname";
+        alert(firstname);
+        document.write(lastname);
+        alert(firstname+" "+lastname);
+        console.log(lastname+", "+firstname);
