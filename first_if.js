9fbc5adf158407db037bdc551b06a5d7aead5cba
Pia <piiseli1956@gmail.com>
2022-01-09 16:59:22 +0100

    taas

---
+++ first_if.js
@@ -0,0 +1,8 @@
+var x = 50;
+var order = 'Beer';
+if (x > 50) {
+document.write("He's over 50!" );
+} else if (x <= 50 && x > 30){
+document.write("Middle aged man, who ordered some "+order);
+} else {
+document.write("It seems you're bit underaged.");}
--- nimet.js
+++ nimet.js
@@ -1,6 +1,6 @@
-var firstname = "Your first name";
-        var lastname = "Your lastname";
-        alert(firstname);
-        document.write(lastname);
-        alert(firstname+" "+lastname);
-        console.log(lastname+", "+firstname);
+var firstname = "Pia";
+var lastname = "Kontiainen";
+out.println(firstname);
+document.write(lastname);
+out.println(firstname+" "+lastname);
+console.log(lastname+", "+firstname);
